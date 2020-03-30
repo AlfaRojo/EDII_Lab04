f9ea266bc8a92560d88f65b364e942a36d7b4786
PabloArreaga <paal241199@gmail.com>
2020-03-30 05:49:01 -0600

    Se agrego Clase Caracter y La clase Zig Zag con los parametros a seguir

---
+++ Cifrados/Modelo/ZigZag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cifrados.Modelo
+{
+	public class ZigZag
+	{
+		Dictionary<int, Caracter> DicNiveles = new Dictionary<int, Caracter>();
+		public void CifrarLectura (string direccion, int niveles)
+		{
+			var ContadorNiveles = 0;
+			while (ContadorNiveles < niveles)
+			{
+				var ListaInicial = new List<byte>();
+				DicNiveles.Add(ContadorNiveles + 1, new Caracter
+				{
+					ListaCaracter = ListaInicial
+				});
+				ContadorNiveles++;
+			}
+
+			var bufferLength = 50;
+			var posicionNivel = 1;
+			var elevador = false;
+
+			// aqui necesito un bufer que mande a llamar al archivo guardado en la API
+		}
+	}
+}
